bf80dcc6631cb46d8906a9d59d14e416380ef881
Same <OsakanaFactory@gmail.com>
2024-01-01 21:27:46 +0900

    Add location markers and change color on map _test

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ import folium
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-# 地図を生成する関数
-def create_map():
-    # 日本の地理的中心の緯度と経度
-    latitude = 36.2048
-    longitude = 138.2529
+# 地名と地理的位置のマッピング
+locations = {
+    '新宿区': {'lat': 35.6938, 'lng': 139.7036},
+    '渋谷区': {'lat': 35.6620, 'lng': 139.7036},
+    # 他の地名と位置もここに追加
+}
 
-    # 地図を作成
-    map_japan = folium.Map(location=[latitude, longitude], zoom_start=5)
+# 特定の地名の色を変更する関数
+def change_color(map, place_name, color):
+    if place_name in locations:
+        lat_lng = locations[place_name]
+        folium.Circle(
+            location=[lat_lng['lat'], lat_lng['lng']],
+            radius=1000,  # 半径
+            color=color,
+            fill=True,
+            fill_color=color
+        ).add_to(map)
 
-    # 地図をHTMLデータとして保存
-    map_html = map_japan._repr_html_()
-    return map_html
+# 地図を作成
+map_japan = folium.Map(location=[35.6895, 139.6917], zoom_start=10)
+
+# 新宿区を赤色に変更する例
+change_color(map_japan, '新宿区', 'red')
+
+# 地図をHTMLファイルとして保存
+map_japan.save('map_of_japan.html')
 
 # アプリケーションウィンドウを作成するクラス
 class MapApp(QWidget):
