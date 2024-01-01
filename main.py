0d440346334dc4457c5f86acd4fa4835f8043f4e
Same <OsakanaFactory@gmail.com>
2024-01-01 21:38:05 +0900

    Add GeoJSON data and update map styling

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import sys
+import json
 import folium
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-# 地名と地理的位置のマッピング
-locations = {
-    '新宿区': {'lat': 35.6938, 'lng': 139.7036},
-    '渋谷区': {'lat': 35.6620, 'lng': 139.7036},
-    # 他の地名と位置もここに追加
-}
+# GeoJSONファイルの読み込み
+with open('/mnt/data/N03-21_13_210101.json', 'r', encoding='utf-8') as file:
+    tokyo_geojson = json.load(file)
 
-# 特定の地名の色を変更する関数
-def change_color(map, place_name, color):
-    if place_name in locations:
-        lat_lng = locations[place_name]
-        folium.Circle(
-            location=[lat_lng['lat'], lat_lng['lng']],
-            radius=1000,  # 半径
-            color=color,
-            fill=True,
-            fill_color=color
+# 特定の地域のデータを抽出する関数
+def extract_area_data(geojson, area_name):
+    return next((feature for feature in geojson['features']
+                 if feature['properties']['N03_004'] == area_name), None)
+
+# 地図を生成する関数
+def create_map():
+    map = folium.Map(location=[35.6895, 139.6917], zoom_start=12, tiles='CartoDB dark_matter')
+    
+    # 新宿区のデータを抽出して地図に適用
+    shinjuku_data = extract_area_data(tokyo_geojson, '新宿区')
+    if shinjuku_data:
+        folium.GeoJson(
+            shinjuku_data,
+            style_function=lambda feature: {
+                'fillColor': 'red',
+                'color': 'red',
+                'weight': 2,
+                'fillOpacity': 0.5
+            }
         ).add_to(map)
 
-# 地図を作成
-map_japan = folium.Map(location=[35.6895, 139.6917], zoom_start=10)
-
-# 新宿区を赤色に変更する例
-change_color(map_japan, '新宿区', 'red')
-
-# 地図をHTMLファイルとして保存
-map_japan.save('map_of_japan.html')
+    return map._repr_html_()
 
 # アプリケーションウィンドウを作成するクラス
 class MapApp(QWidget):
@@ -38,23 +39,16 @@ class MapApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        # レイアウトの設定
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        # 地図を表示するためのWebビュー
         map_view = QWebEngineView()
-
-        # 地図データを取得し、ビューに設定
         map_html = create_map()
         map_view.setHtml(map_html)
-
-        # レイアウトにWebビューを追加
         layout.addWidget(map_view)
 
-        # ウィンドウの設定
         self.setGeometry(100, 100, 800, 600)
-        self.setWindowTitle('日本地図')
+        self.setWindowTitle('新宿区の境界線に色を適用 - ダークテーマ')
         self.show()
 
 # アプリケーションを実行
